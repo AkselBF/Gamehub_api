e2f0cce341328c21c46d1cea6c5216447402a0f6
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-03-15 15:56:38 +0100

    update

--- javascript/games.js
+++ javascript/games.js
@@ -8,23 +8,41 @@ async function getGames() {
 getGames();
 
 function renderGame(game) {
-
+/*
   document.querySelector(".playgames").innerHTML += `
     <a href="../html/procuct.html"><div class="gameImage">
       <img class="playgame" id="playgame1" src="${game.images[0].src}">
       <h3 class="play-price" id="play-price1">${game.price_html}</h3>
     </a>
-  `;
+  `;*/
   
-  /*
-  if (game.attributes[1].name === "Playbox"){
+  
+  if (game.attributes[1].name === "playbox"){
     document.querySelector(".playgames").innerHTML += `
     <a href="../html/procuct.html"><div class="gameImage">
       <img class="playgame" id="playgame1" src="${game.images[0].src}">
       <h3 class="play-price" id="play-price1">${game.price_html}</h3>
     </a>
   `;
-  }*/
+  }
+
+  if (game.attributes[1].name === "playstation"){
+    document.querySelector(".psgames").innerHTML += `
+    <a href="../html/procuct.html"><div class="gameImage">
+      <img class="psgame" id="psgame1" src="${game.images[0].src}">
+      <h3 class="ps-price" id="ps-price1">${game.price_html}</h3>
+    </a>
+  `;
+  }
+
+  if (game.attributes[1].name === "switch"){
+    document.querySelector(".swgames").innerHTML += `
+    <a href="../html/procuct.html"><div class="gameImage">
+      <img class="swgame" id="swgame1" src="${game.images[0].src}">
+      <h3 class="sw-price" id="sw-price1">${game.price_html}</h3>
+    </a>
+  `;
+  }
   
   /*
   if game.category== "switch"{
