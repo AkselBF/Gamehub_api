329fe0afdba343e6ca3e3bfca5bd037cd51b88bf
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-03-15 23:29:15 +0100

    Update product.js

--- javascript/product.js
+++ javascript/product.js
@@ -31,3 +31,43 @@ function renderSwitchGame(switchGame) {
           </a>
   `;
 }*/
+
+/*
+  Chosen game from games.html
+*/
+/*
+console.log(localStorage.getItem("id"));
+
+const detailContainer = document.querySelector(".book_details");
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const gameId = params.get("id");
+
+const url = "https://aks-faret.no/apiex/wp-json/wc/store/products" + gameId;
+*/
+// Gets the chosen game from previous page
+/*
+async function fetchGame() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
+  createHTML(data);
+}
+
+fetchGame();
+*/
+// Get the api data
+/*
+function createHTML(data) {
+  detailContainer.innerHTML = `
+  <h1 class="specific_book_title">${data.results[0].title}</h1>
+  <div class="details_image"
+    style="background-image: url('${data.results[0].formats["image/jpeg"]}')">
+  </div>
+  <div class="details_author">
+    Name: ${data.results[0].authors[0].name}
+    Birth year: ${data.results[0].authors[0].birth_year}
+    Death year: ${data.results[0].authors[0].death_year}
+  </div>`;
+}*/
