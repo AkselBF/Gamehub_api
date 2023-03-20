b6ea23ff137d90566e6225ddcc217204b1ae6f04
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-03-20 12:57:47 +0100

    updates

--- javascript/games.js
+++ javascript/games.js
@@ -1,3 +1,5 @@
+// import { gameId } from "./product";
+
 async function getGames() {
   const res = await fetch("https://aks-faret.no/apiex/wp-json/wc/store/products");
   const data = await res.json();
@@ -7,34 +9,10 @@ async function getGames() {
 
 getGames();
 
-function addGames(game) {
-  const container = document.querySelector(".wrapper");
-  const gameContainer = document.createAttribute("div");
-  const gameLink = document.createAttribute("a");
-
-  const img = document.createAttribute("img");
-
-  gameContainer.append(img);
-  container.append(gameContainer);
-
-  gameLink.href = `html/procuct.html/${game.id}`;
-  gameLink.append(img);
-  gameContainer.append(gameLink);
-
-  /*
-  gameLink.addEventListener("click", () => {
-    localStorage.setItem("id", game.id);
-    console.log(localStorage.getItem("id"));
-  });
-  */
-
-  document.querySelector("body").append(gameContainer);
-}
-
 function renderGame(game) {
   if (game.attributes[1].name === "playbox") {
     document.querySelector(".playgames").innerHTML += `
-    <a href="../html/procuct.html"><div class="gameImage">
+    <a href="./procuct.html?id=${game.id}"><div class="gameImage">
       <img class="playgame" id="playgame1" src="${game.images[0].src}">
       <h3 class="play-price" id="play-price1">${game.price_html}</h3>
     </a>
@@ -43,7 +21,7 @@ function renderGame(game) {
 
   if (game.attributes[1].name === "playstation") {
     document.querySelector(".psgames").innerHTML += `
-    <a href="../html/procuct.html"><div class="gameImage">
+    <a href="./procuct.html?id=${game.id}"><div class="gameImage">
       <img class="psgame" id="psgame1" src="${game.images[0].src}">
       <h3 class="ps-price" id="ps-price1">${game.price_html}</h3>
     </a>
@@ -52,7 +30,8 @@ function renderGame(game) {
 
   if (game.attributes[1].name === "switch") {
     document.querySelector(".swgames").innerHTML += `
-    <a href="../html/procuct.html"><div class="gameImage">
+    <a href="./procuct.html?id=${game.id}
+    "><div class="gameImage">
       <img class="swgame" id="swgame1" src="${game.images[0].src}">
       <h3 class="sw-price" id="sw-price1">${game.price_html}</h3>
     </a>
--- javascript/product.js
+++ javascript/product.js
@@ -1,18 +1,17 @@
 /*
   Chosen game from games.html
 */
-//const gameContainer = document.querySelector(".wrapper");
-//const detailContainer = document.querySelector(".product-detail");
 
 const gameContainer = document.querySelector(".wrapper");
+const detailContainer = document.querySelector(".product-detail");
 
 const queryString = document.location.search;
-const params = new URLSearchParams(document.location.search);
+const params = new URLSearchParams(queryString);
 const gameId = params.get("id");
 
 console.log(gameId);
 
-const url = "https://aks-faret.no/apiex/wp-json/wc/store/products/" + gameId;
+const url = `https://aks-faret.no/apiex/wp-json/wc/store/products/${gameId}`;
 
 // Gets the chosen game from previous page
 async function fetchGame() {
@@ -21,11 +20,15 @@ async function fetchGame() {
   console.log(game);
 
   createHTML(game);
+  createDetails(game);
 }
 
 fetchGame();
 
-// Get the api data
+/*
+  Get the api data
+*/
+
 function createHTML(game) {
   gameContainer.innerHTML = `
   <img class="game" src="${game.images[0].src}">
@@ -39,4 +42,51 @@ function createHTML(game) {
     </h3>
     <h3 class="second-details"></h3>
   </div>`;
+}
+
+function createDetails(game) {
+  detailContainer.innerHTML = `
+    <div class="text1">
+      <div class="left">
+        <h3 class="f-detail">Genre</h3>
+        <p class="s-detail">${game.attributes[4].terms[0].name}</p>
+      </div>
+      <div class="right">
+        <p class="description" id="desc1">${game.description}</p>
+      </div>
+    </div>
+
+    <hr class="text-line">
+
+    <div class="text2">
+      <div class="left">
+        <h3 class="f-detail">Number of players</h3>
+        <p class="s-detail">${game.attributes[5].terms[0].name}</p>
+      </div>
+      <div class="right">
+        <p class="description" id="desc2">
+        </p>
+      </div>
+    </div>
+
+    <hr class="text-line">
+
+    <div class="text3">
+      <div class="left">
+        <h3 class="f-detail">Series</h3>
+        <p class="s-detail">${game.attributes[6].terms[0].name}</p>
+      </div>
+      <div class="right">
+        <p class="description" id="desc3">
+        </p>
+      </div>
+    </div>
+
+    <div class="text4">
+      <div class="left">
+        <h3 class="f-detail">User ratings</h3>
+        <p class="s-detail">${game.attributes[7].terms[0].name}</p>
+      </div>
+    </div>
+  `;
 }
